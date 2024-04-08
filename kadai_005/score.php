58ce755680c16508262e5feff814ebc9d4fe459b
kskbass1030 <153488051+kskbass1030@users.noreply.github.com>
2024-04-08 18:55:58 +0900

    kadai_005の提出

---
+++ kadai_005/score.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="ja">
+  <head>
+    <meta charset="utf-8">
+    <title>平均点計算</title>
+  </head>
+
+  <body>
+    <p>
+      <?php
+      $score1 = 80;
+      $score2 = 60;
+      $score3 = 55;
+      $score4 = 40;
+      $score5 = 100;
+      $score6 = 25;
+      $score7 = 80;
+      $score8 = 95;
+      $score9 = 30;
+      $score10 = 60;
+
+      $total_point = $score1 + $score2 + $score3 + $score4 + $score5 + $score6 + $score7 + $score8 + $score9 + $score10;
+      $average_point = $total_point / 10;
+      echo $average_point;
+      ?>
+    </p>
+  </body>
+</html>
