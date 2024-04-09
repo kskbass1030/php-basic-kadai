568e62b17cd46ffc7bc8467e26293424f382366f
kskbass1030 <153488051+kskbass1030@users.noreply.github.com>
2024-04-09 10:05:58 +0900

    kadai_007の提出

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="ja">
+  <head>
+    <meta charset="utf-8">
+    <title>連想配列</title>
+  </head>
+
+  <body>
+    <p>
+      <?php
+      $onion_info = ['name' => 'onion', 'price' => 200, 'weight' => 160];
+
+      print_r($onion_info);
+      ?>
+    </p>
+  </body>
+</html>
